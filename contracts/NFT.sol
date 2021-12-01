bcb35913773918f72e1b94b9711f56440756a8d9
AliyaDav <aliya_dav98@mail.ru>
2021-12-01 19:58:06 +0100

    acreated first nft

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -8,11 +8,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract propNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() public ERC721("MyNFT", "NFT") {}
+    constructor() public ERC721("propNFT", "NFT") {}
 
     function mintNFT(address recipient, string memory tokenURI)
         public onlyOwner
