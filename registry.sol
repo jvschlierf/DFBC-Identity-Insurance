34e155f45de49903d910066b7065f8b15c61bb6a
jvschlierf <67417254+jvschlierf@users.noreply.github.com>
2021-12-08 10:50:32 +0100

    Added ownership functionality

--- registry.sol
+++ registry.sol
@@ -26,45 +26,61 @@ contract  Registry {
         string codiceFisc;
         string docType;
         string docNumber;
-        bool insured;
-        bool hasNFT; 
+         
     }
+
 
     mapping (address => Owner) owners;
     mapping (Property => Owner) owned_properties;
 
     uint public propertycount;
+    address private Validator = msg.sender();
 
-
-
-    function FirstRegistration (string country, string region, uint zipCode, string city, string street, string streetnum, string addressAdditional, string houseType, uint sqm, uint floor) public { // Add property to 
-       
-        propertycount++;
-        uint id = registeredProperties
-        properties[id] = Property (country, region, zipCode, city, street, addressAdditional, );
-        owned_properties[Property] = msg.sender;
-        // emit AddingLand(landsCount);
-        Verify(id);
+    // Shaurya
+    event PropertyRegistered {  
+    
     }
 
-    function Transfer ()private { //Change in ownership
+    // Shaurya
+    event OwnerRegistered {
 
     }
 
-    function Verify (uint id) private { //Pull update from Oracle (in regular intervals) from the actual public registry
+    Property[] public properties;
+    Owner[] public owners;
+
+
+    function OwnerInformation(uint _id, string memory _firstName, string memory _lastName, string memory _gender, string memory _codiceFiscale, string memory _docType, string memory _docNumber) public ValidateSender {
+        owners.push(Owner(_id, _firstName, _lastName, _gender, _codiceFiscale, _docType, _docNumber));
+    }
+
+    //ownership is verified before construction is called
+    function FirstRegistration(uint _id, uint _areaSqm, uint _floor, uint _zipCode, string memory _country, string memory _region, string memory _city, string memory _street, string memory _streetNumber, string memory _adressAdditional, string memory _houseType) public ValidateSender { // Add property to 
+        properties.push(Property(_id, _areaSqm, _floor, _zipCode, _country, _region, _city, _street, _streetNumber, _adressAdditional, _houseType));
+    }
+
+    modifier ValidateSender {
+        require(msg.sender == Validator);
+        _;
+    }
+
+    function Transfer () public ValidateSender { //Change in ownership - Shaurya 
+
+    // ensure that no NFT is open against the property
 
     }
 
-    function Update () public {  //Change in one of the Property variables
+
+    function Update () public {  //Change in one of the Property variables - Shaurya
 
     }
 
-    function Notify () public { //If verify function has detected fraud, notify customer
+    function Notify () public { //Notify customer here or in python via telegram bot
 
 
     }
 
-    function Remove () public { 
+    function Remove () public { //Remove the property if property  - Jakob to do
 
 
     }
