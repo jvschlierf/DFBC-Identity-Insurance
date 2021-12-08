2800e7f15cd494544e0f1449b6b440f917a53d10
Shaurya Narayan Singh <arsenal2808@gmail.com>
2021-12-08 15:51:05 +0100

    Added 2 events and emits

--- registry.sol
+++ registry.sol
@@ -1,25 +1,26 @@
 //Main Land Registry Contract
 
-pragma solidity  >=0.8.0;
+pragma solidity  ^0.8.0;
 
 contract  Registry {
 
     struct Property {
-        uint id;
+        //uint id;
+        uint sqm;
+        uint floor;
+        uint zipCode;
         string country;
         string region;
-        uint zipCode;
         string city;
         string street;
         string streetnum;
         string addressAdditional;
         string houseType;
-        uint sqm;
-        uint floor;
+
     }
 
     struct Owner {
-        uint id;
+        //uint id;
         string firstName;
         string lastName;
         string gender;  //We accomodate more than 2 genders, so do not use a bool here
@@ -29,9 +30,6 @@ contract  Registry {
          
     }
 	
-	
-
-
     mapping (address => Owner) owners;
     mapping (Property => Owner) owned_properties;
 
@@ -47,28 +45,26 @@ contract  Registry {
         Validator = msg.sender;
     }
 	
+    //keccak256(abi.encodePacked(_str)) -> this creates a unique hash based on arguments passed, might be required if we want a unique identifier for a property/owner
 
-    // Shaurya
-    event PropertyRegistered {  
+    // Creating events of new property and owner
+    event NewPropertyRegistered (uint id, uint areaSqm, uint floor, uint zipCode, string country, string region, string city, string street, string streetNumber, string adressAdditional, string houseType);
+    event NewOwnerCreated (uint id, string firstName, string lastName, string gender, string codiceFiscale, string docType, string docNumber);
     
-    }
-
-    // Shaurya
-    event OwnerRegistered {
-
-    }
-
-    Property[] public properties;
+    //declaring arrays of the two structs created above
+    Property[] public properties; 
     Owner[] public owners;
 
-
-    function OwnerInformation(uint _id, string memory _firstName, string memory _lastName, string memory _gender, string memory _codiceFiscale, string memory _docType, string memory _docNumber) public ValidateSender {
-        owners.push(Owner(_id, _firstName, _lastName, _gender, _codiceFiscale, _docType, _docNumber));
+    function OwnerInformation(string memory _firstName, string memory _lastName, string memory _gender, string memory _codiceFiscale, string memory _docType, string memory _docNumber) public ValidateSender {
+        owners.push(Owner(_firstName, _lastName, _gender, _codiceFiscale, _docType, _docNumber));
+        uint id = owners.push(Owner(_firstName, _lastName, _gender, _codiceFiscale, _docType, _docNumber)) - 1;
+        emit NewOwnerCreated(id, _firstName, _lastName, _gender, _codiceFiscale, _docType, _docNumber);
     }
-
     //ownership is verified before construction is called
-    function FirstRegistration(uint _id, uint _areaSqm, uint _floor, uint _zipCode, string memory _country, string memory _region, string memory _city, string memory _street, string memory _streetNumber, string memory _adressAdditional, string memory _houseType) public ValidateSender { // Add property to 
+    function FirstRegistration(uint _areaSqm, uint _floor, uint _zipCode, string memory _country, string memory _region, string memory _city, string memory _street, string memory _streetNumber, string memory _adressAdditional, string memory _houseType) public ValidateSender { // Add property to 
         properties.push(Property(_id, _areaSqm, _floor, _zipCode, _country, _region, _city, _street, _streetNumber, _adressAdditional, _houseType));
+        uint id = properties.push(Property(_id, _areaSqm, _floor, _zipCode, _country, _region, _city, _street, _streetNumber, _adressAdditional, _houseType)) - 1;
+        emit NewPropertyRegistered(id, _areaSqm, _floor, _zipCode, _country, _region, _city, _street, _streetNumber, _addressadditional, _houseType);
     }
 
 
