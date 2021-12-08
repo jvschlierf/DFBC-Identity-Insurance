82e12f55c35f17d4293a5339fa32625549af5c45
AliyaDav <aliya_dav98@mail.ru>
2021-12-08 11:34:07 +0100

    first bot message

--- telegram-bot/bot.py
+++ telegram-bot/bot.py
@@ -20,23 +20,22 @@ db_keys = r.keys(pattern='*')   # allows us to fetch data
 
 app = Flask(__name__)
 
+
+# def start(update: Update, context: CallbackContext) -> int:
+#     """Starts the conversation"""
+#     reply_keyboard = [['Boy', 'Girl', 'Other']]
+
+#     update.message.reply_text(
+#         '"Hello! This is blockchain-based land registry". Please introduce yourself.'
+#         reply_markup=ReplyKeyboardMarkup(
+#             reply_keyboard, one_time_keyboard=True, input_field_placeholder='Boy or Girl?'
+#         ),
+#     )
+
+#     return GENDER
+
 @app.route('/{}'.format(TOKEN), methods=['POST'])
-
-def start(update: Update, context: CallbackContext) -> int:
-    """Starts the conversation and asks the user about their gender."""
-    reply_keyboard = [['Boy', 'Girl', 'Other']]
-
-    update.message.reply_text(
-        '"Hello! This is blockchain-based land registry". Please introduce yourself.'
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder='Boy or Girl?'
-        ),
-    )
-
-    return GENDER
-
-
-def respond():
+def start():
    update = telegram.Update.de_json(request.get_json(force=True), bot)
 
    chat_id = update.message.chat.id
@@ -48,17 +47,17 @@ def respond():
        bot_welcome = "Hello! This is blockchain-based land registry"
        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
 
-   else:
-       try:
-           text = re.sub(r"\W", "_", text)
-           # create the api link for the avatar based on http://avatars.adorable.io/
-           url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
-           # reply with a photo to the name the user sent,
-           # note that you can send photos by url and telegram will fetch it for you
-           bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
-       except Exception:
-           # if things went wrong
-           bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
+#    else:
+#        try:
+#            text = re.sub(r"\W", "_", text)
+#            # create the api link for the avatar based on http://avatars.adorable.io/
+#            url = "https://api.adorable.io/avatars/285/{}.png".format(text.strip())
+#            # reply with a photo to the name the user sent,
+#            # note that you can send photos by url and telegram will fetch it for you
+#            bot.sendPhoto(chat_id=chat_id, photo=url, reply_to_message_id=msg_id)
+#        except Exception:
+#            # if things went wrong
+#            bot.sendMessage(chat_id=chat_id, text="There was a problem in the name you used, please enter different name", reply_to_message_id=msg_id)
 
    return 'ok'
 
@@ -81,7 +80,7 @@ def respond():
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
-   s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+   s = bot.setWebhook('{APP_URL}{HOOK}'.format(URL=APP_URL, HOOK=TOKEN))
    if s:
        return "webhook setup ok"
    else:
