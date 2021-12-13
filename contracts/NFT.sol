a4307fd376a6db9188a038f437c78f29bddce6ab
Beatrice Guidotti <81224827+beatriceguidotti@users.noreply.github.com>
2021-12-13 14:15:50 +0100

    Update NFT.sol

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -8,47 +8,62 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract propNFT is ERC721, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    uint256 public tokenCounter;
+    uint256 colleteralizedAmount;
+    uint256 totalPrice;
+    enum NFTStatus {
+        UNCOLLATERALIZED,
+        COLLATERALIZED
+    }
     
-    Property[] public properties;
-    mapping(uint => bool) _propertyExists; //check not to issue the same token twice
-    
-    struct Property {
-		address _owner;
-		uint sqm; 
-		string _address;
-		uint value;
-	}
+    // struct Property {
+	// 	address _owner;
+	// 	uint sqm; 
+	// 	string _address;
+	// 	uint value;
+	// } ----------- maybe we will creare a mapping with struct later
 	
-	//mapping PropertyID to owner
-	mapping (uint => address) public tokenToOwner;
-    mapping (ERC721 => address) public NFTtoOwner;
 
-    
-    constructor() ERC721("propNFT", "NFT") public {
-    owner = msg.sender; // ?? 
-    colleteralizedAmount float; // cumulative sum or percentage 
-    totalAmout float; // house price 
+	mapping (uint256 => address) public _tokenToOwner; // althigh in ERC721 we already have a maaping _owners. check if this is necessary
+    mapping (uint256 => uint256) public _tokenColleteralizaion;
+    mapping (uint256 => uint256) public _tokenPrice;
+    event TokenIssued (address owner, uint tokenId);
+    event TokenCollateralized (uint tokenId, uint amount);
+    event TokenPriceSet (uint tokenId, uint amount);
+
+
+    constructor () public ERC721("propNFT", "NFT") {
+        tokenCounter = 0;
     }
 
-    function mintNFT(address memory recipient, string memory tokenURI) 
-        public onlyOwner
-        returns (uint256) 
-        // _mint(owner, tokenId); //DEFINE OWNER
-    {
-    	//require property has not been taken yet
-    	require(!_propertyExists[_color]); //read the value out of the mapping is false, (!false = true)
-					  //i.e. property not taken
-        _tokenIds.increment();
-	    _propertyExists[tokenURI] = true;
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-	
-        return newItemId;
+    function mintNFT(string memory _tokenURI) public returns (uint256) {
+        uint256 _newItemId = tokenCounter;
+        _safeMint(msg.sender, _newItemId);
+        _setTokenURI(_newItemId, tokenURI);
+        _tokenColleteralizaion[_newItemId] = 0;
+        _tokenToOwner[_newItemId] = msg.sender;      
+        tokenCounter = tokenCounter + 1;
+        return _newItemId;
     }
+
+    function tokenURI(uint256 _tokenId) external view returns (string memory);
+
+    // TODO: modifier verifyCollateralized {
+
+    // }
+
+    // TODO: function _collateralize (unit256 _tokenId) public 
+
+    function _setPrice(uint256 _tokenId, uint256 price) public {
+        _tokenPrice[_tokenId] = price;
+    }
+
+    function _checkPrice(uint256 _tokenId) public view returns (uint256) {
+        return _tokenPrice[_tokenId];
+    }
+
+    function _checkCollateralization (uint256 _tokenId) public view returns (uint256) {
+        return _tokenColleteralizaion[_tokenId];
+    }
+
 }
-    modifier verifyCollateralized {
-
-    }
