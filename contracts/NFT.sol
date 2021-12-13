8b1f2ce8a89aca46ff812aba6267d0970cafcf67
AliyaDav <aliya_dav98@mail.ru>
2021-12-13 13:45:51 +0100

    tested nft.sol

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+
 contract propNFT is ERC721, Ownable {
     uint256 public tokenCounter;
     uint256 colleteralizedAmount;
@@ -24,46 +25,56 @@ contract propNFT is ERC721, Ownable {
 	// } ----------- maybe we will creare a mapping with struct later
 	
 
-	mapping (uint256 => address) public _tokenToOwner; // althigh in ERC721 we already have a maaping _owners. check if this is necessary
-    mapping (uint256 => uint256) public _tokenColleteralizaion;
-    mapping (uint256 => uint256) public _tokenPrice;
+	// mapping (uint256 => address) public _tokenToOwner; // althigh in ERC721 we already have a maaping _owners. check if this is necessary
+    mapping (uint256 => uint256) private tokenColleteralizaion;
+    mapping (uint256 => uint256) private tokenPrice;
+    mapping (address => uint256) private OwnerToToken;
+    mapping(uint256 => string) private tokenURIs;
     event TokenIssued (address owner, uint tokenId);
     event TokenCollateralized (uint tokenId, uint amount);
     event TokenPriceSet (uint tokenId, uint amount);
 
 
-    constructor () public ERC721("propNFT", "NFT") {
+    constructor () ERC721("propNFT", "NFT") {
         tokenCounter = 0;
     }
 
-    function mintNFT(string memory _tokenURI) public returns (uint256) {
-        uint256 _newItemId = tokenCounter;
-        _safeMint(msg.sender, _newItemId);
-        _setTokenURI(_newItemId, tokenURI);
-        _tokenColleteralizaion[_newItemId] = 0;
-        _tokenToOwner[_newItemId] = msg.sender;      
+    function mintNFT(string memory tokenURI) public returns (uint256) {
+        uint256 newItemId = tokenCounter;
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        tokenColleteralizaion[newItemId] = 0;
+        OwnerToToken[msg.sender] = newItemId;      
         tokenCounter = tokenCounter + 1;
-        return _newItemId;
+        emit TokenIssued (msg.sender, newItemId);
+        return newItemId;
     }
 
-    function tokenURI(uint256 _tokenId) external view returns (string memory);
-
+    // function tokenURI(uint256 _tokenId) external view returns (string memory);
+    
+    function _setTokenURI(uint256 tokenId, string memory tokenURI) internal {
+        require(_exists(tokenId), "ERC721URIStorage: URI set of nonexistent token");
+        tokenURIs[tokenId] = tokenURI;
+    }
     // TODO: modifier verifyCollateralized {
 
     // }
 
-    // TODO: function _collateralize (unit256 _tokenId) public 
+    // TODO: function _collateralize (unit256 _tokenId, uint256 amount) public {
+    //     emit TokenCollateralized (_tokenId, amount)
+    // }
 
-    function _setPrice(uint256 _tokenId, uint256 price) public {
-        _tokenPrice[_tokenId] = price;
+    function _setPrice(uint256 tokenId, uint256 price) public {
+        tokenPrice[tokenId] = price;
+        emit TokenPriceSet (tokenId, price);
     }
 
-    function _checkPrice(uint256 _tokenId) public view returns (uint256) {
-        return _tokenPrice[_tokenId];
+    function _checkPrice(uint256 tokenId) public view returns (uint256) {
+        return tokenPrice[tokenId];
     }
 
-    function _checkCollateralization (uint256 _tokenId) public view returns (uint256) {
-        return _tokenColleteralizaion[_tokenId];
+    function _checkCollateralization (uint256 tokenId) public view returns (uint256) {
+        return tokenColleteralizaion[tokenId];
     }
 
 }
