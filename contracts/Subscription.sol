5f2ba128a536190501ddfe8ce13778c4b5c0f295
Lzixi <zhu.leonardo@hotmail.it>
2021-12-17 15:50:39 +0100

    added removal of subscribers

--- contracts/Subscription.sol
+++ contracts/Subscription.sol
@@ -11,47 +11,53 @@ import "./Registry.sol";
 contract Subscription is Registry { 
 
     address [] public subscribersList;
-    mapping (address => bool) subscribers;
-    mapping (address => bool) eversubscribed;
-    mapping (address => int) subscribersID;
+    mapping (address => uint) subscribersID;
+    uint subscribers_count;
 
 
     constructor() {
-   
+        subscribers_count = 1;
     } 
 
     uint subscription_price = 10; //at current market value ~$3.7 
 
     event OwnerSubscribed(address owner_address);
     event OwnerUnubscribed(address owner_address);
+    event OwnerSubExpired(address owner_address, string message);
     event lowBalance(string first_name, string last_name);
     
     function subscribe() public isRegistered {
         require(ownerPropertyCount[msg.sender] > 0, "Please register a property before subscribing to this service.");
-        if (eversubscribed[msg.sender]) {
-            subscribers[msg.sender] = true;
-            emit OwnerSubscribed(msg.sender);
-        }
-        else {
-            subscribers[msg.sender] = true;
-            emit OwnerSubscribed(msg.sender);
-            subscribersList.push(msg.sender);
-            eversubscribed[msg.sender] = true;
-        }
-        
+        subscribersList.push(msg.sender);
+        subscribersID[msg.sender] = subscribers_count;
+        subscribers_count ++;
+        emit OwnerSubscribed(msg.sender);       
     }
 
 
     function unsubscribe() public isRegistered {
-        subscribers[msg.sender] = false;
+        uint sub_id = subscribersID[msg.sender];
+        subscribersID[subscribersList[subscribersList.length -1]] = sub_id;
+        subscribersList[sub_id -1 ] = subscribersList[subscribersList.length -1];
+        subscribersList.pop();
+        subscribers_count --;
         emit OwnerUnubscribed(msg.sender);
     }   
 
+    function sub_expired(address to_unsub) public validateSender{
+        uint sub_id = subscribersID[to_unsub];
+        subscribersID[subscribersList[subscribersList.length -1]] = sub_id;
+        subscribersList[sub_id - 1] = subscribersList[subscribersList.length -1];
+        subscribersList.pop();
+        subscribers_count --;
+        emit OwnerSubExpired(to_unsub,"Subscription was cancelled due to insufficient funds");
+
+    }
+
     function subscription_payments () public validateSender { //Function to call once monthly to receive subscription payments
         for (uint i=0; i < subscribersList.length; i++) {
-            if(subscribers[subscribersList[i]] == true) {
-                if(customerBalance[subscribersList[i]] < subscription_price) {  
-                    subscribers[subscribersList[i]] = false;
+            if(customerBalance[subscribersList[i]] < subscription_price) {  
+                    sub_expired(subscribersList[i]);
                 } else if(customerBalance[subscribersList[i]] < subscription_price * 2){
                     uint owner_id = address_to_owner[subscribersList[i]]-1;
                     emit lowBalance(owners[owner_id].firstName,owners[owner_id].lastName);
@@ -61,7 +67,6 @@ contract Subscription is Registry {
                     customerBalance[subscribersList[i]] -= subscription_price;
                     revenue += subscription_price;
                 }
-        }
         }
     }
 
