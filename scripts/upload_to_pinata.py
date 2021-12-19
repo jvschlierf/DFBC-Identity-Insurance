cf9712970e90bb3ed5bcd566e068712445fd31e3
Beatrice Guidotti <81224827+beatriceguidotti@users.noreply.github.com>
2021-12-19 23:28:08 +0100

    Update upload_to_pinata.py

--- scripts/upload_to_pinata.py
+++ scripts/upload_to_pinata.py
@@ -1,13 +1,24 @@
+import requests
 import os
+from pathlib import Path
+from dotenv import load_dotenv
 
-PINATA_BASE_URL = 'https://app.pinata.cloud'
-endpoint = '/pinning/pinFileToIPFS'
+load_dotenv()
 
-filename = '' # should be the input from the user
+PINATA_BASE_URL = 'https://api.pinata.cloud/'
+endpoint = 'pinning/pinFileToIPFS'
 
-headers = {'pinata_api_key': os.getenv('PINATA_API_KEY'),
-            'pinata_secret_api_key': os.getenv('PINATA_API_SECRET') 
-}
+headers = {'pinata_api_key': API_Key,
+           'pinata_secret_api_key': API_Secret}
 
+def upload_to_pinata(filepath, filename):
+    with Path(filepath).open("rb") as fp:
+        response = requests.post(PINATA_BASE_URL + endpoint,
+                                 files={"file": (filename, fp)},
+                                 headers=headers)
 
-
+        ipfs_hash = response.json()['IpfsHash']
+        filename = filepath.split("/")[-1:][0]
+        uri = "https://ipfs.io/ipfs/{}?filename={}".format(ipfs_hash, filename)
+        
+    return uri
