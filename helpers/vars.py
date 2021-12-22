7caaba3c1ccf7da0b7a919e7a108501f6ebb3d5a
Lzixi <zhu.leonardo@hotmail.it>
2021-12-22 19:04:48 +0100

    added functions and vars

---
+++ helpers/vars.py
@@ -0,0 +1,6 @@
+default_account_address = '0x5face5582CaE06bE3E51B05DA2c3853D353A5CC5'
+url = 'https://ropsten.infura.io/v3/5fc49ffa7ebe42f282366f4a13230f1e'
+
+registry_address = '0x99c1ecA0e4F90ecaCCa011112409B0f9442395dB'
+nft_address = '0xCA6eef5c9419e8Fb40d904CC99CffE433Bb3F6A1'
+
